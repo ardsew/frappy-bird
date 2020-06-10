36e01684323ebb9f5377e653dba3d242c875a4ea
Andrew Kim <andrewkim260@gmail.com>
2020-06-09 18:51:47 -0700

    Parallax moving pipes, score counter increments, object collision is detected.

---
+++ Assets/scripts/CountdownText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class CountdownText : MonoBehaviour
+{
+
+    public delegate void CountdownFinished();
+    public static event CountdownFinished OnCountdownFinished;
+    Text countdown;
+
+    void OnEnable() {
+        countdown = GetComponent<Text>();
+        countdown.text = "3";
+        StartCoroutine("Countdown");
+    }
+
+    IEnumerator Countdown() {
+        for(int i = 3; i >= 0; i--) {
+            countdown.text = i.ToString();
+            yield return new WaitForSeconds(1);
+        }
+        OnCountdownFinished();
+    }
+}
--- Assets/scripts/TapController.cs
+++ Assets/scripts/TapController.cs
@@ -5,23 +5,54 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class TapController : MonoBehaviour {
 
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+
     public float tapForce = 10, tiltSmooth = 5;
     public Vector3 startPos;
 
     new Rigidbody2D rigidbody;
     Quaternion downRotation, forwardRotation;
 
+    GameManager game;
+
     void Start() {
         rigidbody = GetComponent<Rigidbody2D>();
         // converts a 3d vec to 4d aka Quaternion
         downRotation = Quaternion.Euler(0,0,-90);
         forwardRotation = Quaternion.Euler(0,0,35);
+        game = GameManager.Instance;
+        rigidbody.simulated = false;
+    }
+
+    void OnEnable() {
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+    void OnDisable() {
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+    }
+
+    void OnGameStarted() {
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.simulated = true;
+    }
+
+    void OnGameOverConfirmed() {
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
     }
 
     void Update() {
+        if(game.GameOver) return;
+
         // equiv. to Left Click or a Tap on Android
         if(Input.GetMouseButtonDown(0)) {
             transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
             // can also later try ForceMode2D.Impulse
             rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
         }
@@ -34,6 +65,7 @@ public class TapController : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D col) {
         if(col.gameObject.tag == "ScoreZone") {
             // register a score, play a sound
+            OnPlayerScored(); // event sent to GAmeManager
         }
 
         if(col.gameObject.tag == "DeadZone") {
@@ -41,7 +73,7 @@ public class TapController : MonoBehaviour {
             // freeze bird when he hits something
             rigidbody.simulated = false;
             // register a dead event, and play a sound
-
+            OnPlayerDied(); // same as above;
         }
     }
 }
